672203abcb68e4a75baad436d9db6a87852e1094
sixleaveakkm <teki.go@finatext.com>
2020-07-17 00:55:50 +0900

    upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # check-permission
+
+```yaml
+steps:
+  - name: "check permission to repo"
+    uses: "sixleaveakkm/check-permission"
+    with:
+      permission: "write"
+      token: ${{ secrets.CI_TOKEN }}
+      repos: ""
+```
+
+https://developer.github.com/v3/repos/collaborators/#get-repository-permissions-for-a-user
---
+++ index.ts
@@ -0,0 +1,31 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+const perms = ["none", "read", "write", "admin"];
+
+(async () => {
+  const repos = core.getInput('repos').split(',')
+  const token = core.getInput('token');
+  const targetPermission = core.getInput('permission')
+
+  const octokit = github.getOctokit(token)
+  const response = await octokit.users.getAuthenticated()
+  const username = response.data.login
+
+  for ( const rep of repos ) {
+    const tuple = rep.split("/")
+    const owner = tuple[0]
+    const repo = tuple[1]
+    const res = await octokit.repos.getCollaboratorPermissionLevel({
+      owner: owner,
+      repo: repo,
+      username: username
+    })
+    const actualPerm = res.data.permission
+    core.debug(`permission for ${rep} is ${actualPerm}`)
+    if (perms.indexOf(actualPerm) < perms.indexOf(targetPermission)) {
+      core.setFailed(`permission for ${rep} not fit required`)
+      return
+    }
+  }
+})()
